da119b2f1a94bf23f83e8b7a0724a45e39ac4201
Ashley <ashley.bergsma42@gmail.com>
2020-07-15 15:53:45 -0700

    building architecture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import "./App.css";
+import './App.css';
+import React, { useState, useEffect } from 'react' ;
+import axios from 'axios';
+import { BASE_URL } from './base_constants/index';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+export default function App() {
+    const [nasa, setNasa] = useState([]); 
+
+//     useEffect(() => {
+//         axios.get('https://api.nasa.gov/planetary/apod?api_key=DmqHDWfkfTBQlUPcUQrjqRxiaJSrhaRKRjacfKDV')
+//         .then(res => {
+//             console.log(res.data)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//     })
+// }, [])
+
+return null;
 }
-
-export default App;
